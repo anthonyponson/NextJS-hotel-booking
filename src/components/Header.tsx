15a36dc206bcc007a0ed7f92da9d0ebc7dbf5865
Anthony Ponson <antonyponson@gmail.com>
2024-01-26 19:40:37 +0530

    popup added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,13 @@
 'use client'
-import { ChevronDown, HomeIcon, Menu } from 'lucide-react'
+import {
+  ChevronDown,
+  DiameterIcon,
+  GitCommitVerticalIcon,
+  HomeIcon,
+  Menu,
+  Phone,
+  Plane,
+} from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Fragment, useState } from 'react'
@@ -12,7 +20,31 @@ function Header() {
       href: '#',
       icon: HomeIcon,
     },
+    {
+      name: 'Book a Stay',
+      description: 'Get a better understanding of your traffic',
+      href: '#',
+      icon: Plane,
+    },
+    {
+      name: 'Book a Stay',
+      description: 'Get a better understanding of your traffic',
+      href: '#',
+      icon: GitCommitVerticalIcon,
+    },
   ]
+
+  const callTOAction = [
+    {
+      name: 'See Demo Booking',
+      icon: DiameterIcon,
+    },
+    {
+      name: 'contact',
+      icon: Phone,
+    },
+  ]
+
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
@@ -49,15 +81,42 @@ function Header() {
               leave='transition ease-in duration-150'
               leaveFrom='opacity-100 translate-y-0'
               leaveTo='opacity-0 translate-y-1'
-            ></Transition>
+            >
+              <Popover.Panel className='absolute bg-white -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl shadow-lg ring-gray-900/5'>
+                <div className='p-4'>
+                  {products.map(pro => (
+                    <div className='group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50 '>
+                      <div className='flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-gray-200'>
+                        <pro.icon className='h-6 w-6 text-blue-700' />
+                      </div>
+                      <div className='flex-auto'>
+                        <a
+                          href={pro.href}
+                          className='block font-semibold text-[#013b94]'
+                        >
+                          {pro.name}
+                          <span className='absolute inset-0' />
+                        </a>
+                        <p className='mt-1 text-[#013B94]'>{pro.description}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
 
-            <Popover.Panel className='absolute bg-white -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl shadow-lg ring-gray-900/5'>
-              {products.map(pro => (
-                <>
-                  <h1>{pro.name}</h1>
-                </>
-              ))}
-            </Popover.Panel>
+                <div className='grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50'>
+                  {callTOAction.map(item => (
+                    <a
+                      key={item.name}
+                      href='#'
+                      className='flex items-center justify-center hover:bg-gray-100 gap-5 py-3'
+                    >
+                      <item.icon className='h-6 w-6 flex-none text-[#013b94]' />
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </Popover.Panel>
+            </Transition>
           </Popover>
         </Popover.Group>
       </nav>
